8c7830d89d0defe87b76dfed123a01c36c406766
Ssaad548 <syedsaad547@gmail.com>
2025-03-14 15:11:20 +0800

    Save progress

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,17 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+import { url } from "inspector";
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  url: import.meta.env.VITE_APPWRITE_API_URL,
+};
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+export const account = new Account();
+export const databases = new Databases();
+export const storage = new Storage();
+export const avatars = new Avatars();
